33be9b8a4e188ab39f8dba8bee2a9073b8df5f80
domingobishop <domingobishop@gmail.com>
2020-10-13 16:07:46 +0100

    aws proxy check

--- src/setThemeGlobals.php
+++ src/setThemeGlobals.php
@@ -20,6 +20,7 @@ function setThemeGlobals($environment = null) {
             $pre_crumbs = array('Legal' => '/');
             break;
         case 'external':
+        case 'aws_public':
             $pre_crumbs = array(
                 'Legal' => '/legal/'
             );
